512057dfbd8840086d756e5820acaddf723aaf7b
Crowdbotics <team@crowdbotics.com>
2022-05-23 02:54:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "deutscher_real_esta_35386",
+  url: "https://deutscher_real_esta_35386.botics.co",
+  api: "https://deutscher_real_esta_35386.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
